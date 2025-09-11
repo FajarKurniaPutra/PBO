42a11fa79bf599a19a84467069eb64aa87a89def
Fajar Kurnia Putra <fajarkurniaputrasib@gmail.com>
2025-09-11 10:00:47 +0700

    Commit 1: Jobsheet 3

---
+++ jobsheet3/Anggota.java
@@ -0,0 +1,58 @@
+public class Anggota {
+    private String noKTP;
+    private String nama;
+    private int limitPinjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String noKTP, String nama, int limitPinjaman) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0; 
+    }
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int uang) {
+        if (jumlahPinjaman + uang > limitPinjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += uang;
+        }
+    }
+
+    public void angsur(int uang) {
+        if (jumlahPinjaman == 0) {
+            System.out.println("Tidak ada pinjaman yang perlu diangsur.");
+            return;
+        }
+
+        int minimalAngsuran = (int) (0.1 * jumlahPinjaman);
+
+        if (uang < minimalAngsuran) {
+            System.out.println("Maaf, angsuran harus minimal 10% dari jumlah pinjaman (" + minimalAngsuran + ").");
+        } else {
+            if (uang >= jumlahPinjaman) {
+                jumlahPinjaman = 0;
+                System.out.println("Pinjaman lunas!");
+            } else {
+                jumlahPinjaman -= uang;
+                System.out.println("Terima kasih, angsuran diterima.");
+            }
+        }
+    }
+}
---
+++ jobsheet3/MotorDemo.java
@@ -0,0 +1,33 @@
+public class MotorDemo {
+    public static void main(String[] args) {
+        Motor motor1 = new Motor();
+        motor1.displayStatus();
+
+        motor1.setPlatNomor("B 0838 XZ");
+        motor1.setKecepatan(50);
+        motor1.displayStatus();
+
+        Motor motor2 = new Motor();
+        motor2.setPlatNomor("N 9840 AB");
+        motor2.setIsMesinOn(true);
+        motor2.setKecepatan(40);
+        motor2.displayStatus();
+
+        Motor motor3 = new Motor();
+        motor3.setPlatNomor("D 8343 CV");
+        motor3.setKecepatan(60);
+        motor3.displayStatus();
+
+        Motor motor4 = new Motor();
+        motor4.setPlatNomor("P 1554 AZ");
+        motor4.setIsMesinOn(true);
+        motor4.setKecepatan(110);
+        motor4.displayStatus();
+
+        Motor motor5 = new Motor();
+        motor5.setPlatNomor("S 4937 IP");
+        motor5.setIsMesinOn(true);
+        motor5.setKecepatan(-10);
+        motor5.displayStatus();
+    }
+}
